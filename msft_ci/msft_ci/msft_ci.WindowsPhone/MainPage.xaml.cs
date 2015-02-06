70cf1e8eab2adf17285d54fc9f85a5443d13b3c4
anniruddh.koppal <anniruddh_koppal@yahoo.com>
2015-02-06 15:51:16 -0500

    Universal app with push notification

---
+++ msft_ci/msft_ci/msft_ci.WindowsPhone/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace msft_ci
+{
+    public sealed partial class MainPage : Page
+    {
+        private void LayoutRoot_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            Login();
+        }
+    }
+}
